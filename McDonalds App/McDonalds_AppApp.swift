88422873451371045bb4411e714f5d5ade425b67
Bradlee King <kingofbrad@gmail.com>
2024-09-22 23:10:03 +0100

    Initial Commit

---
+++ McDonalds App/McDonalds_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  McDonalds_AppApp.swift
+//  McDonalds App
+//
+//  Created by Bradlee King on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct McDonalds_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
